7fae90183365834617c609279d7b5808b35ac646
Hernán R <hernan@null.net>
2015-07-24 03:44:27 -0430

    Fixed html injection vulnerability in note viewer, improved style

--- src/templates/note_viewer.php
+++ src/templates/note_viewer.php
@@ -1,6 +1,10 @@
-<div class="page-block">
+<?php 
+	$content = htmlentities($note->content);
+	$content = nl2br($content);		
+?>
+<div class="page-block view">
 	<h1 class="view" id="title"><?= $note->title ?></h1>
 	<div id="text-content">
-		<p><?= $note->content ?></p>
+		<p><?= $content; ?></p>
 	</div>
 </div>
