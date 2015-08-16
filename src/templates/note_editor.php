4ef48ef4dff32107fa7640cb536512c8ccd61944
Hernán R <hernan@null.net>
2015-08-16 16:26:46 -0430

    Updated 'templates'

--- src/templates/note_editor.php
+++ src/templates/note_editor.php
@@ -18,29 +18,29 @@ if(isset($note) && isset($_GET["key"])) {
 	</div>
 
 	<div id="login-form">
-		<p>¿Tienes una cuenta?</p>
-		<input type="text" placeholder="Usuario" />
-		<input type="password" placeholder="Contraseña" />
-		<input type="checkbox" id="remember-box" checked="checked" /> <label for="remember-box"> Recordarme</label>
+		<p><?=$STR["have_account_prompt"]?></p>
+		<input type="text" placeholder="<?=$STR["username"]?>" />
+		<input type="password" placeholder="<?=$STR["password"]?>" />
+		<input type="checkbox" id="remember-box" checked="checked" /> <label for="remember-box"> <?=$STR["remember_me"]?></label>
 
-		<button class="btn f-right">Entrar</button>
-		<a class="f-left" id="register-link" href="javascript:void(0)">Crear una cuenta</a>
+		<button class="btn f-right"><?=$STR["login_button"]?></button>
+		<a class="f-left" id="register-link" href="javascript:void(0)"><?=$STR["register_link"]?></a>
 	</div>
 
 	<div id="register-form" class="hidden">
-		<p>Nombre de usuario</p>
-		<input type="text" placeholder="Nombre de usuario" />
-		<p>Contraseña</p>
-		<input type="password" placeholder="Contraseña" />
-		<p>Repite tu contraseña</p>
-		<input type="password" placeholder="Contraseña, de nuevo" />
+		<p><?=$STR["username"]?></p>
+		<input type="text" placeholder="<?=$STR["username"]?>" />
+		<p><?=$STR["password"]?></p>
+		<input type="password" placeholder="<?=$STR["password"]?>" />
+		<p><?=$STR["repeat_password"]?></p>
+		<input type="password" placeholder="<?=$STR["password"]?>" />
 
-		<button class="btn f-right">Registrar</button>
-		<a id="cancel-register" href="javascript:void(0)" class="f-left">&#8678; Cancelar</a>
+		<button class="btn f-right"><?=$STR["register_button"]?></button>
+		<a id="cancel-register" href="javascript:void(0)" class="f-left">&#8678; <?=$STR["cancel"]?></a>
 	</div>
 	<br class="clearfix" />
 	<div class="status-box clearfix hidden">
-		<i class="icon warning"></i><span>We are yet to bleed, all of the time and energy. Half awake and almost dead. Keeping empty beds elsewhere</span>
+		<i class="icon warning"></i><span></span>
 	</div>
 
 </div>
@@ -51,12 +51,12 @@ if(isset($note) && isset($_GET["key"])) {
 		<textarea id="text-area" class="input"><?= (isset($note) ? $note->content : "") ?></textarea>
 	</div>
 	<div id="bottom-actions">
-		<input type="checkbox" id="private-chk" checked="<?php isset($note) ? ($note->private ? "checked" : "") : "" ?>" name="private"><label for="private-chk"> Nota privada</label>
-		<button class="flat-btn" id="save-btn">Guardar</button>
+		<input type="checkbox" id="private-chk" checked="<?php isset($note) ? ($note->private ? "checked" : "") : "" ?>" name="private"><label for="private-chk"> <?=$STR["private_note"]?></label>
+		<button class="flat-btn" id="save-btn"><?=$STR["save"]?></button>
 	</div>
 	<div <?php if(isset($note)) { ?> style="display:block" <?php } ?> id="action-links">
-	<a id="delete-link" href="javascript:void(0)">Eliminar</a>
-	| <a id="share-link" href="javascript:void(0)">Compartir</a>
+	<a id="delete-link" href="javascript:void(0)"><?=$STR["delete"]?></a>
+	| <a id="share-link" href="javascript:void(0)"><?=$STR["share"]?></a>
 	</div>
 
 	<div id="share-url">
