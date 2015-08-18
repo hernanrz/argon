ad6d55e6b80feebd7499ec26ac165905a11b8535
Hernán R <hernan@null.net>
2015-08-17 22:36:39 -0430

    Updated sample settings file

--- src/settings/settings_sample.php
+++ src/settings/settings_sample.php
@@ -5,6 +5,30 @@
 
 include "database.php";
 
-define("AR_FOLDER", "");//Contains the folder name where the project is in, leave blank if it's at the top of the webroot, should always end with a /, unless it's left blank
-define("AR_PATH", $_SERVER["HTTP_HOST"] ."/". AR_FOLDER); //Used in the <base/> tag
+
+//Used for includes
+define("AR_ROOT",  dirname(__FILE__));
+
+//Contains the folder name where the project is in, leave blank if it's at the top of the webroot, should always end with a /, unless it's left blank
+define("AR_FOLDER", "argon/src/");
+
+//Used in the <base/> tag
+define("AR_PATH", $_SERVER["HTTP_HOST"] ."/". AR_FOLDER);
+
+$AR_AVAILABLE_LANGS = [
+  'es' => 'Español'
+];
+
+//Defines the language the program is going to use
+//Default is spanish, for now
+$AR_LANG = "es";
+
+if(isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
+  $lang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+  if(isset($AR_AVAILABLE_LANGS[$lang])) {
+    $AR_LANG = $lang;
+  }
+}
+
+require AR_ROOT. "/../lang/{$AR_LANG}_locale.php";
 ?>
